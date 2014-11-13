4194f6ad38e7c2553b417a78315ed918e6e14d89
rdmckinnon <ryan@ryanmckinnon.com>
2014-11-12 21:03:24 -0500

    Validations and testing added

---
+++ db/rubychalleng.rb
@@ -0,0 +1,17 @@
+#Ruby challange 
+
+# class Pets("name", "legs")
+
+	def initialize (name, legs)
+		@name = name 
+		@legs = legs
+	end
+
+	def give_name
+		return @name
+	end
+
+	def get_name
+		return @legs
+	end
+
