8aef33fd2d8dd4427207ab39127659b55df3bd26
rdmckinnon <ryan@ryanmckinnon.com>
2014-11-17 21:00:10 -0500

    Added market scaffold and lots others

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -8,7 +8,7 @@ class ProductTest < ActiveSupport::TestCase
   	assert product.invalid?
   	assert product.errors[:title].any?
   	assert product.errors[:description].any?
-	assert product.errors[:image_url].any?
+	  assert product.errors[:image_url].any?
   	assert product.errors[:price].any?
   end
   
@@ -16,11 +16,31 @@ class ProductTest < ActiveSupport::TestCase
 
   test "product must have a unique title" do
   	product = Product.new(title: products(:ruby).title, 
-				description: "random text"
+				description: "random text",
 				price: 1,
 				image_url: "meme.gif")
 		assert product.invalid?
 		assert_equal ["has already been taken"], product.errors[:title]
 	end
+
+  test "price must be positive" do
+    product = Product.new (title: "random text",
+          desctiption: "more random stuff",
+          image_url: "blabla.gif"      )
+
+    product.price = -1
+    assert product.invalid?
+    asset_equal ["Must be greater or equal to 0.01"], 
+      product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 1
+    assert product.vaild?
+  end
+
 
 end
