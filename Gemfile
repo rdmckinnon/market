6011a818eb6e2a2c2fd18a62e8f19b702b3fd771
rdmckinnon <ryan@ryanmckinnon.com>
2014-11-21 14:33:45 -0500

    Potential CSS fixes

--- Gemfile
+++ Gemfile
@@ -38,3 +38,7 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
+# gem 'bootstrap-sass', '~> 3.2.0'
+# gem 'autoprefixer-rails'
+
+gem 'twitter-bootstrap-rails', '~> 3.2.0'
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_item
 
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -8,4 +11,17 @@ class Product < ActiveRecord::Base
 		message: "Must be a gif|jpg|png "
 	}
 	 
+
+	 private
+
+	 def ensure_not_referenced_by_any_line_item
+	 	if line_items.empty?
+	 		retrun true
+	 	else
+	 		errors.add(:base, "line item present")
+	 		return false
+	 	end
+	 	
+	 		
+ 	end
 end
